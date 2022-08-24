7dba62e76fd975b6014641d5786eac19beacd7e4
AngelMaldonado <amaldonadojuarez@gmail.com>
2022-08-24 18:07:52 -0500

    🐝#2 - Algoritmo Optimización de Partículas en MATLAB

---
+++ 01Inteligencia de Enjambre/PSO-Octave/Sphere.m
@@ -0,0 +1,9 @@
+%% https://www.youtube.com/watch?v=xPkRL_Gt6PI
+%% Minimización de la función f(x)=||x||^2, donde x es un vector x∈R
+%% Angel de Jesús Maldonado Juárez
+
+function z = Shpere(x)
+
+  z = sum(x.^2);
+
+end
